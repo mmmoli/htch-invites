4f8af2d9b8cee9474a2a62f9e833526fa9683154
Michele Memoli <michele@100shapes.com>
2022-10-26 23:50:00 +0100

    Pimped-out args

--- invites_cli/src/args.rs
+++ invites_cli/src/args.rs
@@ -10,41 +10,40 @@ pub struct InvitesArgs {
 
 #[derive(Debug, Subcommand)]
 pub enum EntityType {
-    InvitationList(InvitationListArgs),
+    Subdomain(SubdomainArgs),
     Invitation(InvitationArgs),
 }
 
 #[derive(Debug, Args)]
 pub struct InvitationArgs {
     #[clap(subcommand)]
-    pub operation: InvitationListOperation,
+    pub operation: InvitationOperation,
 }
 
-
 #[derive(Debug, Args)]
-pub struct InvitationListArgs {
+pub struct SubdomainArgs {
     #[clap(subcommand)]
-    pub operation: InvitationListOperation,
-};
-
+    pub operation: SubdomainOperation,
+}
 
 #[derive(Debug, Subcommand)]
-pub enum InvitationListOperation {
-    /// Create a new Invitation List
-    Create(CreateArgs),
-    /// List all Invitations Lists
-    List(ListArgs),
+pub enum SubdomainOperation {
+    /// Create a new Invitation List.
+    Create(CreateSubdomainArgs),
     /// Delete Invitation List
-    Delete(DeleteArgs),
+    Delete(DeleteSubdomainArgs),
 }
 
 #[derive(Debug, Args)]
-pub struct CreateArgs {
-    /// The email address of the Recipient.
-    pub email: String,
+pub struct CreateSubdomainArgs {
+    /// The id for the new Subdomain.
+    pub id: String,
+}
 
-    /// The Subdomain the Recipient will be invited to.
-    pub subdomain: String,
+#[derive(Debug, Args)]
+pub struct DeleteSubdomainArgs {
+    /// The id for the Subdomain to delete.
+    pub id: String,
 }
 
 #[derive(Debug, Args)]
@@ -53,5 +52,33 @@ pub struct ListArgs;
 #[derive(Debug, Args)]
 pub struct DeleteArgs {
     /// the ID of the Invitation List to delete
+    pub id: String,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum InvitationOperation {
+    Create(CreateInvitationArgs),
+    Send(SendInvitationArgs),
+    Revoke(RevokeInvitationArgs),
+}
+
+#[derive(Debug, Args)]
+pub struct CreateInvitationArgs {
+    /// The email address of the Recipient.
+    pub id: String,
+
+    /// The Subdomain the Recipient will be invited to.
+    pub subdomain: String,
+}
+
+#[derive(Debug, Args)]
+pub struct SendInvitationArgs {
+    /// the ID of the Invitation to send.
+    pub id: String,
+}
+
+#[derive(Debug, Args)]
+pub struct RevokeInvitationArgs {
+    /// the ID of the Invitation to revoke.
     pub id: String,
 }
