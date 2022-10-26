bca031bd09661021abd9239708e7b406bcba28c6
Michele Memoli <michele@100shapes.com>
2022-10-26 23:09:48 +0100

    Stubbed an API server

--- invites_api/src/main.rs
+++ invites_api/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/world")]
+fn world() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build()
+        .mount("/hello", routes![world])
+        .launch()
+        .await?;
+
+    Ok(())
 }
