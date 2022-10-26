c8b7356b8e02a161d7fba7333f659d65940ccdda
Michele Memoli <michele@100shapes.com>
2022-10-26 21:53:35 +0100

    proj layout tweaks

---
+++ invites_shared/src/invites.rs
@@ -0,0 +1,148 @@
+mod traits {
+    use super::models::invite_states::RevokedInvitation;
+    use crate::{Recipient, Subdomain};
+
+    pub trait Invite {
+        fn recipient(&self) -> &Recipient;
+        fn subdomain(&self) -> &Subdomain;
+    }
+
+    pub trait Revokable {
+        fn revoke(self) -> RevokedInvitation;
+    }
+}
+
+pub mod models {
+    use self::invite_states::{DraftedInvitation, SentInvitation};
+
+    #[derive(Debug)]
+    pub struct Recipient(String);
+
+    #[derive(Debug)]
+    pub struct Subdomain(String);
+
+    #[derive(Debug)]
+    pub struct Invitation {
+        recipient: Recipient,
+        subdomain: Subdomain,
+    }
+
+    impl Invitation {
+        pub fn create(recipient: Recipient, subdomain: Subdomain) -> DraftedInvitation {
+            DraftedInvitation {
+                recipient,
+                subdomain,
+            }
+        }
+
+        pub fn send(self) -> SentInvitation {
+            SentInvitation {
+                recipient: self.recipient,
+                subdomain: self.subdomain,
+            }
+        }
+    }
+
+    pub mod invite_states {
+        use super::{Recipient, Subdomain};
+        use crate::invites::traits::{Invite, Revokable};
+
+        #[derive(Debug)]
+        pub struct DraftedInvitation {
+            pub(crate) recipient: Recipient,
+            pub(crate) subdomain: Subdomain,
+        }
+
+        impl Invite for DraftedInvitation {
+            fn recipient(&self) -> &Recipient {
+                &self.recipient
+            }
+
+            fn subdomain(&self) -> &Subdomain {
+                &self.subdomain
+            }
+        }
+
+        impl Revokable for DraftedInvitation {
+            fn revoke(self) -> RevokedInvitation {
+                RevokedInvitation {
+                    recipient: self.recipient,
+                    subdomain: self.subdomain,
+                }
+            }
+        }
+
+        #[derive(Debug)]
+        pub struct SentInvitation {
+            pub(crate) recipient: Recipient,
+            pub(crate) subdomain: Subdomain,
+        }
+
+        impl Invite for SentInvitation {
+            fn recipient(&self) -> &Recipient {
+                &self.recipient
+            }
+
+            fn subdomain(&self) -> &Subdomain {
+                &self.subdomain
+            }
+        }
+
+        impl Revokable for SentInvitation {
+            fn revoke(self) -> RevokedInvitation {
+                RevokedInvitation {
+                    recipient: self.recipient,
+                    subdomain: self.subdomain,
+                }
+            }
+        }
+
+        #[derive(Debug)]
+        struct AcceptedInvitation {
+            recipient: Recipient,
+            subdomain: Subdomain,
+        }
+
+        impl Invite for AcceptedInvitation {
+            fn recipient(&self) -> &Recipient {
+                &self.recipient
+            }
+
+            fn subdomain(&self) -> &Subdomain {
+                &self.subdomain
+            }
+        }
+
+        #[derive(Debug)]
+        struct ExpiredInvitation {
+            recipient: Recipient,
+            subdomain: Subdomain,
+        }
+
+        impl Invite for ExpiredInvitation {
+            fn recipient(&self) -> &Recipient {
+                &self.recipient
+            }
+
+            fn subdomain(&self) -> &Subdomain {
+                &self.subdomain
+            }
+        }
+
+        #[derive(Debug)]
+        pub struct RevokedInvitation {
+            recipient: Recipient,
+            subdomain: Subdomain,
+        }
+
+        impl Invite for RevokedInvitation {
+            fn recipient(&self) -> &Recipient {
+                &self.recipient
+            }
+
+            fn subdomain(&self) -> &Subdomain {
+                &self.subdomain
+            }
+        }
+    }
+}
--- invites_shared/src/invites/mod.rs
+++
@@ -1,3 +0,0 @@
-mod invites;
-pub mod models;
-mod traits;
